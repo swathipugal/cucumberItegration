9b65a51e1e858b0e00e09894a7fbd34a78268fe9
lavanya <lavanya.ganabathi@accenture.com>
2019-09-26 12:20:23 +0530

    commit

--- src/test/java/skeleton/Stepdefs.java
+++ src/test/java/skeleton/Stepdefs.java
@@ -26,7 +26,7 @@ public class Stepdefs {
 		driver.findElement(By.name("userName")).sendKeys("Lalitha");
 		driver.findElement(By.name("password")).sendKeys("Password123");
 		driver.findElement(By.name("Login")).click();
-		System.out.println("print");
+		System.out.println("printll");
 	}
 	 
 	
